84c01c782ea77682482e22e98e3788fae2711821
trussel4 <46553657+trussel4@users.noreply.github.com>
2019-02-25 20:22:39 -0700

    Assignment 2 Problem 1

---
+++ Assignment2/src/Calculator.java
@@ -0,0 +1,46 @@
+/*
+Name: Travis Russell
+Cse360 Assignment 2
+ID: 1210982198
+The following is problem 1 from the assignment with the comments
+added and no code changes made.
+*/
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	//returns the total value in the calculator thus far
+	public int getTotal () {
+		return 0;
+	}
+	
+	//will add values given an int (value)
+	public void add (int value) {
+
+	}
+	
+	//will subtract values given an int (value)
+	public void subtract (int value) {
+
+	}
+	
+	//will multiply values given an int (value)
+	public void multiply (int value) {
+
+	}
+	
+	//will divide values given an int (value)
+	public void divide (int value) {
+
+	}
+	
+	//will return the history of the calculator
+	public String getHistory () {
+		return "";
+	}
+
+}
