f0b10f5a71a359991f4cdf2143b3d0ecab2d639e
trussel4 <46553657+trussel4@users.noreply.github.com>
2019-02-25 20:28:15 -0700

    Assignment 2 Problem 3

--- Assignment2/src/Calculator.java
+++ Assignment2/src/Calculator.java
@@ -2,17 +2,20 @@
 Name: Travis Russell
 Cse360 Assignment 2
 ID: 1210982198
-The following is problem 1 from the assignment with the comments
-added and no code changes made.
+The following is problem 3 from the assignment with all past functions
+and the get-history function also added
 */
 
 public class Calculator {
 
 	private int total;
+	StringBuffer past;
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		past = new StringBuffer("0"); //to keep track of past calculations
 	}
+	
 	//returns the total value in the calculator thus far
 	public int getTotal () {
 		return total;
@@ -21,16 +24,19 @@ public class Calculator {
 	//will add values given an int (value)
 	public void add (int value) {
 		total = total + value;
+		past.append("+" + value);
 	}
 	
 	//will subtract values given an int (value)
 	public void subtract (int value) {
 		total = total - value;
+		past.append("-" + value);
 	}
 	
 	//will multiply values given an int (value)
 	public void multiply (int value) {
 		total = total * value;
+		past.append("*" + value);
 	}
 	
 	//will divide values given an int (value)
@@ -40,11 +46,12 @@ public class Calculator {
 			total = 0;
 		}
 		else total = total / value;
+		past.append("/" + value);
 	}
 	
 	//will return the history of the calculator
 	public String getHistory () {
-		return "";
+		return past.toString();
 	}
 
 }
